1499c10c01fa4f1d9958f2e9dd9f98d3a04cec9b
Lezhandre <50102924+Lezhandre@users.noreply.github.com>
2021-04-13 22:14:05 +0300

    first update
    added file input
    removed exceptions (error: No matching conversion for functional-style cast from 'const char [24]' to 'std::exception')

--- lab1 SIAOD/lab1 SIAOD.cpp
+++ lab1 SIAOD/lab1 SIAOD.cpp
@@ -1,24 +1,29 @@
-﻿#define USE_MATH_DEFINES
+#define USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 #include <vector>
 #include <queue>
+#include <list>
+#include <map>
+#include <fstream>
+#include <exception>
+#include <algorithm>
 
 using namespace std;
 
-void gen(int n, const vector<vector<double>>& graph, unsigned w, unsigned w2, double len, double now_len = 0, vector <int> ans = {}){
-    ans.push_back(w);
-    if (w == w2) {
+void gen(const vector<vector<double>>& graph, unsigned start, unsigned end, double len, double cur_len = 0, vector <unsigned> ans = {}){
+    ans.push_back(start);
+    if (start == end) {
         cout << "Path: ";
-        for (int i = 0; i < ans.size(); ++i){
+        for (unsigned i = 0; i < ans.size(); ++i){
             cout << ans[i] + 1 << ' ';
         }
-        cout << '\n';
+        cout << endl;
         return;
     }
-    for (int i = 0; i < n; ++i){
-        if (graph[w][i] > 0 && now_len + graph[w][i] <= len){
-            gen(n, graph, i, w2, len, now_len + graph[w][i], ans);
+    for (unsigned i = 0; i < graph.size(); ++i){
+        if (graph[start][i] > 0 && cur_len + graph[start][i] <= len){
+            gen(graph, i, end, len, cur_len + graph[start][i], ans);
         }
     }
 }
@@ -28,176 +33,120 @@ void BellmanFord(const vector<vector<double>>& graph, unsigned start, unsigned e
     queue <unsigned> x({ start });
     vector <double> dp(n);
     for (unsigned i = 0; i < n; ++i){
-        dp[i] = -1;
+        dp[i] = INFINITY;
     }
     dp[start] = 0;
     while (!x.empty()){
         unsigned w = x.front();
         for (unsigned i = 0; i < n; ++i){
-            if (graph[w][i] > 0) {
-                if (dp[i] < 0 || dp[i] > dp[w] + graph[w][i]) {
-                    x.push(i);
-                    dp[i] = dp[w] + graph[w][i];
-                }
+            if (dp[i] > dp[w] + graph[w][i]) {
+                x.push(i);
+                dp[i] = dp[w] + graph[w][i];
             }
         }
         x.pop();
     }
-    if (dp[end] >= 0 && !isinf(dp[end])){
+    if (!isinf(dp[end])){
         cout << "Length of the shortest way: " << dp[end] << endl;
-        gen(n, graph, start, end, dp[end]);
+        gen(graph, start, end, dp[end]);
     }
     else
-        cout << "There isn't way" << endl;
+        cout << "There isn't any path" << endl;
 }
 
 void Prima(const vector<vector<double>>& graph) {
     unsigned n = graph.size();
-    vector<bool> used (n);
-    vector<double> min_e (n, INFINITY);
-    vector <unsigned> sel_e (n, -1);
-    vector<vector<unsigned>> tree(n);
+    vector<bool> used(n);
+    vector<double> min_e(n, INFINITY);
+    vector <unsigned> sel_e(n, -1);
     min_e[0] = 0;
+    double ans = 0;
     for (unsigned i = 0; i < n; ++i) {
-        unsigned v = 0;
-        for (; v < n; ++v){
-            if (!used[v]) break;
-        }
+        unsigned v;
+        for (v = 0; v < n && used[v]; ++v);
         for (unsigned j = v + 1; j < n; ++j) {
-            if (!used[j] && (v == -1 || min_e[j] < min_e[v])) {
+            if (!used[j] && min_e[j] < min_e[v]) {
                 v = j;
             }
         }
         if (isinf(min_e[v])) {
-            cout << "No MST!" << endl;
+            cout << "No MSTree!" << endl;
             exit(0);
         }
+        ans += min_e[v];
         used[v] = true;
         for (unsigned to = 0; to < n; ++to){
-            if (used[to]) continue;
-            if (graph[v][to] < min_e[to]) {
+            if (!used[to] && graph[v][to] < min_e[to]) {
                 min_e[to] = graph[v][to];
-			    sel_e[to] = v;
+                sel_e[to] = v;
             }
         }
     }
-    double ans = 0;
+    cout << "Weight of MSTree: " << ans << endl;
+    cout << "Edges of MSTree:" << endl;
     for (unsigned i = 1; i < sel_e.size(); ++i) {
-        cout << sel_e[i] + 1 << " <-> " << i + 1 << endl;
-        ans += min_e[i];
+        cout << '(' << sel_e[i] + 1 << ", " << i + 1 << ')' << endl;
     }
-    cout << "Weight of tree: " << ans << endl;
 }
+
+struct edge{
+    unsigned i, j;
+    double weight;
+};
 
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    unsigned size, start, end;
-    cout << "Enter number of vertices in your graph: ";
-    try {
-        cin >> size;
-        if (!cin) throw exception("Wrong format in number of vertices!");
+    unsigned size = 0, start, end;
+    ifstream fin("graph.txt");
+    vector <edge> tmp_vec;
+    while (true){
+        unsigned tmp_j, tmp_k;
+        double tmp_w;
+        fin >> tmp_j >> tmp_k >> tmp_w;
+        if (!fin) break;
+        size = max(max(tmp_j, tmp_k), size);
+        tmp_vec.push_back({tmp_j - 1, tmp_k - 1, tmp_w});
     }
-    catch (exception& bad) {
-        cout << bad.what() << endl;
-        return -1;
-    }
+    fin.close();
     vector<vector<double>> matrix(size, vector<double>(size));
     for (auto& i : matrix)
         for (double& x : i)
             x = INFINITY;
     for (unsigned i = 0; i < size; ++i)
         matrix[i][i] = 0;
-    cout << "Enter quantity of edges: ";
+    for (unsigned i = 0; i < tmp_vec.size(); ++i){
+        matrix[tmp_vec[i].i][tmp_vec[i].j] = tmp_vec[i].weight;
+    }
+    unsigned choice;
     try {
-        cin >> size;
-        if (!cin) throw exception("Wrong format in number of vertices!");
+        cout << "Select algorithm:" << endl;
+        cout << "1) Bellman-Ford" << endl;
+        cout << "2) Prima" << endl;
+        cin >> choice;
+        if (!cin || choice == 0 || choice > 2) throw("Wrong selection format!");
     }
-    catch (exception& bad) {
-        cout << bad.what() << endl;
-        return -1;
+    catch (string& bad) {
+        cout << bad << endl;
+        return -2;
     }
-    cout << "Enter edges:" << endl;
-    for (unsigned i = 0; i < size; ++i){
-        unsigned j, k;
-        double weight;
-        try {
-            cin >> j >> k >> weight;
-            matrix[j - 1][k - 1] = weight;
-            if (!cin) throw exception("Wrong format in one of vertices!");
-        }
-        catch (exception& bad) {
-            cout << bad.what() << endl;
-            return -2;
-        }
-    }
-    size = matrix.size();
+    if (choice == 2) goto PRIMA;
     try {
         cout << "Enter start and end: ";
         cin >> start >> end;
         --start; --end;
-        if (!cin || start >= size || end >= size) throw exception("Wrong start/end format!");
+        if (!cin || start >= size || end >= size) throw("Wrong start/end format!");
     }
-    catch (exception& bad) {
-        cout << bad.what()<<endl;
+    catch (string& bad) {
+        cout << bad << endl;
         return -2;
     }
     BellmanFord(matrix, start, end);
-    for (unsigned i = 0; i < size; ++i) {
-        for(unsigned j = 0; j <= i; ++j){
-            if (!isinf(matrix[i][j])) {
-                matrix[j][i] = matrix[i][j];
-            }
-            else if (!isinf(matrix[j][i])) {
-                matrix[i][j] = matrix[j][i];
-            } else {
-                matrix[i][j] = INFINITY;
-                matrix[j][i] = INFINITY;
-            }
-        }
-        matrix[i][i] = INFINITY;
+    return 0;
+    PRIMA:
+    for (unsigned i = 0; i < tmp_vec.size(); ++i){
+        matrix[tmp_vec[i].j][tmp_vec[i].i] = tmp_vec[i].weight;
     }
-    cout << endl << "Graph for Prima:" << endl;
-    for (unsigned i = 0; i < size; ++i) {
-        for (unsigned j = 0; j < size; ++j) {
-            cout << matrix[i][j] << '\t';
-        }
-        cout << endl;
-    }
-    cout << endl;
     Prima(matrix);
+    return 0;
 }
-
-/*
-12
-0 2 3 23 45 6 2 3 0 3 2 4
-0 0 3 23 45 6 2 3 0 3 2 5
-0 2 0 23 45 6 2 3 0 3 2 5
-0 2 3 0 45 61 2 0 3 2 43 5
-0 2 3 53 0 9 2 3 0 2 44 5
-0 2 3 3 8 0 2 3 0 3 62 5
-0 2 3 3 4 5 0 3 0 3 2 5
-0 2 3 23 5 6 2 0 0 34 2 5
-0 8 3 12 45 6 2 3 0 3 4 5
-0 0 3 21 45 6 2 3 0 0 4 5
-0 1 3 25 45 6 2 3 0 0 4 5
-7 58 3 3 4 4 3 8 99 9 9 0
-*/
-
-/*
-14
-1 4 3
-1 5 2
-2 5 2
-5 10 3
-4 8 2
-4 9 7
-9 12 1
-8 11 4
-10 12 2
-10 13 1
-6 13 5
-6 7 3
-3 7 2
-2 6 4
-*/
